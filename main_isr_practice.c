54ec77845696dc6c7bc8fd475e10369aa1acda7b
Henrique-Albuquerque2 <henriqueaa1@al.insper.edu.br>
2025-02-18 15:16:35 -0300

    new

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -11,13 +11,6 @@ volatile int btn_flag;
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
     btn_flag = 1;
-
-    // ERRO: addon IsrNoLoop
-    while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
-    }
-    // ERRO: addon IsrNoDelay
   }
 }
 
